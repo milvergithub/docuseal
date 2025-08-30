b0f9e562c81327b41bffac2ad33c74002436f6f2
milver <milver@gmail.com>
2025-08-30 17:35:30 -0400

    first initial commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,84 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import TelegramBot from 'node-telegram-bot-api';
+import { Web3Storage } from 'web3.storage';
+import * as fs from 'node:fs';
+import { writeFileSync } from 'fs';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { join } from 'path';
 
-@Controller()
+@Controller('/api/v1/documents')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private bot: TelegramBot;
+  constructor(private readonly appService: AppService) {
+    //ME 8459400352:AAEe9OPCYRRmFlpr9MjqHbYipIrqW3XAcbI
+    // 8396574258:AAHnwgp_We0-yXwJCRHDFX5gJjH_UrbsV5M
+    const token = '8459400352:AAEe9OPCYRRmFlpr9MjqHbYipIrqW3XAcbI';
+    this.bot = new TelegramBot(token, { polling: true });
 
-  @Get()
+    // Listener de mensajes de texto
+    this.bot.on('message', (msg) => {
+      console.log(msg);
+      if (msg.text) {
+        this.bot.sendMessage(msg.chat.id, 'Hola 👋, recibí tu mensaje');
+      }
+    });
+  }
+
+  @Get('')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // 👉 Endpoint para subir un archivo y enviarlo a Telegram
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    // Guardar archivo en disco temporalmente
+
+    const filePath = join(__dirname, '..', '..', 'uploads', file.originalname);
+
+    writeFileSync(filePath, file.buffer);
+
+    // Aquí defines a qué chat lo mandas (por ejemplo, tu propio ID)
+    const chatId = '630724184';
+
+    this.bot.on('message', (msg) => {
+      console.log('Tu chatId es:', msg.chat.id);
+    });
+
+    // Enviar archivo al chat de Telegram
+    await this.bot.sendDocument(chatId, filePath);
+
+    return { message: 'Archivo enviado a Telegram', file: file.originalname };
+  }
+
+  getAccessToken() {
+    return process.env.WEB3STORAGE_TOKEN; // pon tu token en variables de entorno
+  }
+
+  makeStorageClient() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-call
+    return new Web3Storage({ token: this.getAccessToken() });
+  }
+
+  async storeDocument(path) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const client = this.makeStorageClient();
+    const content = await fs.promises.readFile(path);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const files = [new File([content], 'documento.pdf')];
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    const cid = await client.put(files);
+    console.log('📦 Subido a Filecoin/IPFS con CID:', cid);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return cid;
   }
 }
